3bf0823f2e935be69731b85557b4d6f27be6dbdc
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-04-06 14:07:29 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+basic.forever(function () {
+    basic.showNumber(input.compassHeading())
+    if (input.compassHeading() < 45 || input.compassHeading() > 315) {
+        music.playMelody("C5 B A G F E D C ", 120)
+    } else {
+        music.playMelody("C5 B A G F E D C ", 120)
+    }
+})
